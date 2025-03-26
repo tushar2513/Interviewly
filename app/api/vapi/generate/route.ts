3f7f91bbc4a35cab95468f82670113cbe81fbfa4
tushar <tusharpol825@gmail.com>
2025-03-26 14:11:23 +0530

    added vapi voice agent

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -2,6 +2,7 @@ import {generateText} from "ai";
 import {google} from "@ai-sdk/google"
 import { getRandomInterviewCover } from "@/lib/utils";
 import { db } from "@/firebase/admin";
+
 export async function GET() {
     return Response.json({success: true, data: 'Thank You'}, {status: 200})
 }
